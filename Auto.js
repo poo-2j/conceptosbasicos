e45a18d80cc3a3e1eb59ce8505dbe6886fc0f69c
CiriloBatista <cirilo.8471574@gmail.com>
2019-02-08 15:29:14 -0600

    Se agregaron mas metodos

---
+++ Auto.js
@@ -0,0 +1,35 @@
+export default class Auto{
+    constructor(marca, modelo, color="blanco", kilometraje=0){
+        this._marca = marca;
+        this._modelo = modelo;
+        this._color = color;
+        this._kilometraje = kilometraje;
+        this._estado = "Apagado";
+    }
+    get color(){
+        return this._color;
+    }
+    set color(newColor){
+        this._color = newColor.toUpperCase();
+    }
+    mostrarEstado(){
+        console.log(`El auto ${this._marca} ${this._color} del año ${this._modelo} tiene un kilometraje de ${this._kilometraje}kms y está ${this._estado}`);
+    }
+    encender(){
+        this._estado = "Encendido";
+        this.mostrarEstado();
+    }
+    apagar(){
+        this._estado = "Apagado";
+        this.mostrarEstado();
+    }
+    avanzar(velocidad, tiempo){
+        if(this._estado === "Apagado"){
+            console.log("El Auto no puede avanzar porque está apagado");
+        }else{
+            let distancia = velocidad * tiempo;
+            this._kilometraje = this._kilometraje + distancia;
+            this.mostrarEstado();
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log('Hola Mundo!');
+import Auto from "./Auto.js";
+// Datos del Auto: (marca, modelo, color, kilometraje)
+let auto1 = new Auto("Ford", 2000, "rojo", 100);
+let auto2 = new Auto("Nissan", 2019, "azul");
+let auto3 = new Auto("Range Rover", 2018);
+
+console.log(`Color ${auto1.color}`);//Lectura
+console.log(`Color ${auto2.color}`);//Lectura
+auto1.color = "Amarillo";//Escritura
+auto3.color = "Negro";//Escritura
+
+auto1.mostrarEstado();
+auto2.mostrarEstado();
+auto3.mostrarEstado();
+
+auto1.encender();
+auto1.avanzar(50, 2);
+auto1.apagar();
+auto1.avanzar(20, 1);
